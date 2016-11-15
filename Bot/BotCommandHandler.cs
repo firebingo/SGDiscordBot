9a00f672e7177c0510027f2565af04c4852dc314
Matthew <matthew.t.kides@gmail.com>
2016-11-15 10:38:09 -0700

    Function to calculate user message counts. Untested.

--- Bot/BotCommandHandler.cs
+++ Bot/BotCommandHandler.cs
@@ -1,7 +1,10 @@
 ﻿using Discord;
 using Discord.Commands;
+using MySql.Data.MySqlClient;
+using SGMessageBot.DataBase;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,6 +49,106 @@ namespace SGMessageBot.Bot
 					Environment.Exit(0);
 				}
 			});
+
+			Client.GetService<CommandService>().CreateCommand("messagecount")
+			.Alias("messagecount")
+			.Parameter("UserToCalc", ParameterType.Optional)
+			.Do(async e =>
+			{
+				var hasPerm = false;
+				foreach (var permRole in SGMessageBot.BotConfig.credInfo.commandRoleIds)
+				{
+					var checkMatch = e.User.Roles.Where(r => r.Id == permRole).FirstOrDefault();
+					if (checkMatch != null)
+						hasPerm = true;
+				}
+				if (hasPerm)
+				{
+					var userArg = e.GetArg(0);
+					var result = await calculateMessageCounts(userArg);
+					if(userArg == null || userArg.Trim() == String.Empty)
+					{
+						result = result.OrderBy(x => x.messageCount).ToList();
+						if (result.Count == 0)
+							await e.Channel.SendMessage($"No users have sent a message on this server.");
+						else
+						{
+							var mostCount = result.FirstOrDefault();
+							await e.Channel.SendMessage($"User with most messages: {mostCount.userMention} with {mostCount.messageCount} messages.");
+						}
+					}
+					else
+					{
+						if (result.Count == 0)
+							await e.Channel.SendMessage($"User {userArg} has not sent any messages.");
+						else
+						{
+							var userCount = result.FirstOrDefault();
+							await e.Channel.SendMessage($"User {userCount.userMention} has sent {userCount.messageCount} messages.");
+						}
+					}
+				}
+			});
 		}
+
+		#region Calc Functions
+		private static async Task<DateTime> getEarlistMessage()
+		{
+			var result = new DateTime();
+
+			return Task.FromResult<DateTime>(result).Result;
+		}
+
+		private static async Task<List<UserCountModel>> calculateMessageCounts(string user)
+		{
+			var queryString = "";
+			List<UserCountModel> results = new List<UserCountModel>();
+			if (user == null || user.Trim() == String.Empty)
+			{
+				queryString = "SELECT messages.userID, usersinservers.nickNameMention, count(messages.userID) from messages LEFT JOIN usersinservers ON messages.userID=usersinservers.userID WHERE usersinservers.serverID=messages.serverID GROUP BY userID";
+				DataLayerShortcut.ExecuteReader<List<UserCountModel>>(readMessageCounts, results, queryString);
+			}
+			else
+			{
+				queryString = "SELECT messages.userID, usersinservers.nickNameMention, count(messages.userID) from messages LEFT JOIN usersinservers ON messages.userID=usersinservers.userID WHERE usersinservers.serverID=messages.serverID AND usersinservers.nickNameMention=@mention GROUP BY userID";
+				DataLayerShortcut.ExecuteReader<List<UserCountModel>>(readMessageCounts, results, queryString, new MySqlParameter("@mention", user));
+			}
+			return Task.FromResult<List<UserCountModel>>(results).Result;
+		}
+		#endregion
+
+		#region Data Readers
+		private static void readEarliestDate(IDataReader reader, DateTime data)
+		{
+			reader = reader as MySqlDataReader;
+			if (reader != null)
+			{
+				while (reader.Read())
+				{
+					data = reader.GetDateTime(0);
+				}
+			}
+		}
+
+		private static void readMessageCounts(IDataReader reader, List<UserCountModel> data)
+		{
+			reader = reader as MySqlDataReader;
+			if (reader != null)
+			{
+				while (reader.Read())
+				{
+					if (reader.FieldCount >= 3)
+					{
+						var userObject = new UserCountModel();
+						ulong? temp = reader.GetValue(0) as ulong?;
+						userObject.userID = temp.HasValue ? temp.Value : 0;
+						userObject.userMention = reader.GetString(1);
+						userObject.messageCount = reader.GetInt32(2);
+						data.Add(userObject);
+					}
+				}
+			}
+		}
+		#endregion
 	}
 }
---
+++ Bot/BotModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGMessageBot.Bot
+{
+	[Serializable]
+	public class UserCountModel
+	{
+		public ulong userID;
+		public string userMention;
+		public int messageCount;
+	}
+}
